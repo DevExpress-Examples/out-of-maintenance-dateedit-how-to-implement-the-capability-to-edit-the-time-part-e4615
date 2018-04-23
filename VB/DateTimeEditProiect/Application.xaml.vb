93c403c50f1381d72be8bb67858702e464e75efd
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/DateTimeEditProiect/Application.xaml.vb
@@ -0,0 +1,21 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Configuration
+Imports System.Data
+Imports System.Linq
+Imports System.Threading
+Imports System.Windows
+
+Namespace DateTimeEditProiect
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Partial Public Class App
+        Inherits Application
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+    End Class
+End Namespace
